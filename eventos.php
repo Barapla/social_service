061e541eaab82fb4cebf351632ec48fe6d32da82
Barapla <araujobryan333@gmail.com>
2023-08-30 02:09:52 -0700

    add a layout.php

--- eventos.php
+++ eventos.php
@@ -1,47 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PRIISMA</title>
-  <link href="./plugins/src/css/output.css" rel="stylesheet">
-</head>
-<body>
-    <!-- Navbar partial -->
-    <div class="h-full w-full">
-      <?php include "./plugins/partials/navbar.html" ?>
-    </div>
-    
-    <!-- Hero partial -->
-    <?php include "./plugins/partials/heroEvents.html" ?>
 
-    <!-- Raffle partial -->
-    <?php include "./plugins/partials/raffle.php" ?>
+<?php
+    // Initialize the $page_content variable with an empty string
+    $page_content = "";
 
-    <!-- Contact partial -->
-    <?php include "./plugins/partials/contact.html" ?>
+    // Append the content of individual includes to the $page_content
+    $page_content .= '<!-- Hero partial -->' . file_get_contents("../plugins/partials/heroEvents.html");
+    $page_content .= '<!-- Raffle partial -->' . file_get_contents("../plugins/partials/raffle.html");
+    $page_content .= '<!-- Contact partial -->' . file_get_contents("../plugins/partials/contact.html");
+    $page_content .= '<!-- Modal partial -->' . file_get_contents("../plugins/partials/modal.html");
+    $page_content .= '<script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>';
 
-    <!-- Modal partial -->
-    <?php include "./plugins/partials/modal.html" ?>
-
-    <!-- Footer partial -->
-    <?php include "./plugins/partials/footer.html" ?>
-
-    
-</body>
-<!-- Scripts -->
-<!-- <script>
-    const Item = document.getElementById("eventos_li");
-    const text = Item.firstChild;
-    if (Item) {
-      Item.style.backgroundColor = "#9a1d78";
-      text.style.color = "#9a1d78";
-    } else {
-      console.error("Element with ID '" + id + "' not found!");
-    }
-</script> -->
-<script src="./plugins/src/js/navbar.js"></script>
-<script src="./plugins/src/js/modal.js"></script>
-<script src="./plugins/src/js/stats.js"></script>
-<script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
-</html>
+    include('./layout.php');
+  ?>
---
+++ layout.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRIISMA</title>
+    <link href="./plugins/src/css/output.css" rel="stylesheet">
+</head>
+<body>
+    <!-- Navbar partial -->
+    <div class="h-full w-full">
+        <?php include "./plugins/partials/navbar.html" ?>
+    </div>
+
+    <!-- Main content -->
+    <main>
+        <?php
+            if (isset($page_content)) {
+                echo $page_content;
+            }
+        ?>
+    </main>
+
+    <!-- Footer partial -->
+    <?php include "./plugins/partials/footer.html" ?>
+</body>
+<!-- Scripts -->
+<script src="./plugins/src/js/navbar.js"></script>
+<script src="./plugins/src/js/modal.js"></script>
+<script src="./plugins/src/js/workshops.js"></script>
+<script src="./plugins/src/js/testimonial.js"></script>
+<script src="./plugins/src/js/stats.js"></script>
+</html>
