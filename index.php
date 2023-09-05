cbb5fdce51da09cad31bb1f171843bcc5cc2c44c
JulyAn1234 <l20211790@tectijuana.edu.mx>
2023-09-05 14:40:07 -0700

    replaced areas_index/areas.php for areas_index.html

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     $page_content .= '<!-- Hero partial -->' . file_get_contents("./plugins/partials/imageSlider.html");
     $page_content .= '<!-- Band partial -->' . file_get_contents("./plugins/partials/band_index.html");
     $page_content .= '<!-- Stats partial -->' . file_get_contents("./plugins/partials/stats.html");
-    $page_content .= '<!-- Areas partial -->' . file_get_contents("./plugins/partials/areas_index/areas.php");
+    $page_content .= '<!-- Areas partial -->' . file_get_contents("./plugins/partials/areas_index.html");
     $page_content .= '<!-- Area partial -->' . file_get_contents("./plugins/partials/areas.html");
     $page_content .= '<!-- Workshop partial -->' . file_get_contents("./plugins/partials/workshops.html");
     $page_content .= '<!-- Testimonial partial -->' . file_get_contents("./plugins/partials/testimonial.html");
